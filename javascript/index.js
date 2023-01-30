da1496bf188dfa236acf6797e6f459b8acf58a43
Maik Schmidt <maikschmidt91@gmail.com>
2023-01-30 15:37:04 +0100

    Modified iteration 2 so it's not a callback hell, but working with the return value now - finally got it thanks to JS | Promises on student portal...

--- javascript/index.js
+++ javascript/index.js
@@ -57,57 +57,59 @@ getInstruction("mashedPotatoes", 0, (step0) => {
 } )
 
 // Iteration 2 - using promises
+
 obtainInstruction("steak", 0)
   .then((step0) => {
     document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
-    obtainInstruction("steak", 1)
-      .then((step1) => {
-        document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
-        obtainInstruction("steak", 2)
-          .then((step2) => {
-            document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
-            obtainInstruction("steak", 3) 
-              .then((step3) => {
-                document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
-                obtainInstruction("steak", 4) 
-                  .then((step4) => {
-                    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
-                    obtainInstruction("steak", 5)
-                      .then((step5) => {
-                        document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
-                        obtainInstruction("steak", 6) 
-                          .then((step6) => {
-                            document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
-                            document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`;
-                            document.querySelector("#steakImg").removeAttribute("hidden");
-                          })
-                          .catch((error6) => {
-                            console.log(error6);
-                          })
-                      })
-                      .catch((error5) => {
-                        console.log(error5);
-                      })
-                  })
-                  .catch((error4) => {
-                    console.log(error4);
-                  })
-              })
-              .catch((error3) => {
-                console.log(error3);
-              })
-          })
-          .catch((error2) => {
-            console.log(error2);
-          })
-      })
-      .catch((error1) => {
-        console.log(error1);
-      })
+    return obtainInstruction("steak", 1);
   })
-  .catch((error0) => {
-    console.log(error0);
+  .catch((error) => {
+    console.log(error);
   })
+  .then((step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+    return obtainInstruction("steak", 2);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+    return obtainInstruction("steak", 3);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+    return obtainInstruction("steak", 4);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+    return obtainInstruction("steak", 5);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
+    return obtainInstruction("steak", 6);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
+    document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
 
 // Iteration 3 using async/await
 async function makeBroccoli() {
